f6bd84e0d5bd0443133414add8ddf47e9a9161d6
yldztlcit <166502156+yldztlcit@users.noreply.github.com>
2026-03-12 17:18:18 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:18:17.984Z
-Commit: Add multi-language support
+Timestamp: 2026-03-12T14:18:19.379Z
+Commit: Resolve null pointer exception in user service
