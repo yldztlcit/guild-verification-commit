7114ca210e8aa9544d3f9276fd06eb16d9f4607b
yldztlcit <166502156+yldztlcit@users.noreply.github.com>
2026-03-12 17:18:31 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:18:19.379Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-12T14:18:32.445Z
+Commit: Add export functionality for reports
